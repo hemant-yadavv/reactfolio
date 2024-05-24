37690e0833af64bc83eca486be6e6ccc930eda19
Hemant <hemantyadav1137@gmail.com>
2024-05-24 15:51:49 +0530

    created components card and tours

--- plan-with-love/src/App.js
+++ plan-with-love/src/App.js
@@ -1,9 +1,33 @@
+import { useState } from 'react';
 import './App.css';
+import Tours from './components/Tours';
+import data from './data'
 
 function App() {
+
+  const [tours,setTours] = useState(data);
+
+  function removeTour(id){
+    const newTours = tours.filter(tour => tour.id !== id);
+    setTours(newTours);
+  }
+
+  if(tours.length === 0){
+    return (
+      <div className="refresh">
+        <h2>No Tours left</h2>
+        <button className='btn-white' onClick={()=> setTours(data)}>
+          Refresh
+        </button>
+      </div>
+    );
+  }
+
   return (
-    <div>Hello World</div>
-  );
+    <div className='app'>
+      <Tours tours={tours} removeTour={removeTour}></Tours>
+    </div>
+  ); 
 }
 
 export default App;
