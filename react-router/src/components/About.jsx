f1647736095f971d4d5dcc133e6af78d7ea5a2ee
Hemant <hemantyadav1137@gmail.com>
2024-06-17 20:23:17 +0530

    useNagivate hook

--- react-router/src/components/About.jsx
+++ react-router/src/components/About.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const About = () => {
+  const navigate = useNavigate();
+
+  function clickHandler(){
+    navigate('/support')
+  }
+
+  function backHandler(){
+    navigate(-1);
+  }
+
   return (
     <div>
-      About
+      <div>
+        This is Abut Page
+      </div>
+
+      <button onClick={clickHandler}>
+        Move to Support Page
+      </button>
+
+      <button onClick={backHandler}>
+        Go Back
+      </button>
     </div>
   )
 }
--- react-router/src/components/Lab.jsx
+++ react-router/src/components/Lab.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Lab = () => {
+  const navigate = useNavigate();
+
+  function clcikHandler(){
+    navigate('/about');
+  }
+
   return (
     <div>
-      Lab
+      <div>
+        This is Lab page
+      </div>
+      <button onClick={clcikHandler}>
+        Move to About Page
+      </button>
     </div>
   )
 }
