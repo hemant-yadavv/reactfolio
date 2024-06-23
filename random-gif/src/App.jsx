572a265e30b830a690bca54cc35b50e8a4d7f6c3
Hemant <hemantyadav1137@gmail.com>
2024-06-23 21:03:53 +0530

    gif generator project setup

---
+++ random-gif/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>Random Gif</div>
+    </>
+  )
+}
+
+export default App
---
+++ random-gif/vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { loadEnv } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    define: {
+      'process.env.REACT_APP_GIPHY_API_KEY': JSON.stringify(env.REACT_APP_GIPHY_API_KEY)
+    },
+    plugins: [react()],
+  }
+})
